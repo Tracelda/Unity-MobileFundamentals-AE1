286c4e383249947f12f35a0f195ab2a86f54fbb5
Edward Goodhew <ejgoodhew@btinternet.com>
2018-10-18 16:43:50 +0100

    move work done to animations and SFXs

--- DCA525 Mobile Game Fundamentals/Assets/AnimationScrpt.cs
+++ DCA525 Mobile Game Fundamentals/Assets/AnimationScrpt.cs
@@ -10,28 +10,71 @@ public class AnimationScrpt : MonoBehaviour {
     public float RayLength;
 
     private Rigidbody2D CharRigid;
-    private Animator CharAnim;
+    public Animator CharAnim;
+    bool Sprint;
 
     // Use this for initialization
     void Start () {
         CharRigid = GetComponent<Rigidbody2D>();
         CharAnim = GetComponent<Animator>();
+        
     }
 	
 	// Update is called once per frame
-	void Update () {
-        CharRigid.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MoveSpeed, CharRigid.velocity.y);
-        CharAnim.SetFloat("MovementSpeed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+	void Update ()
+    {
+        if (Input.GetButtonDown("Fire3"))
+        {
+            Sprint = true;
+
+            if (Input.GetButtonUp("Fire3"))
+            {
+                Sprint = false;
+            }
+        }
+        else
+            Sprint = false;
+
+        if (Sprint == true)
+        {
+            CharRigid.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MoveSpeed + 2, CharRigid.velocity.y);
+            CharAnim.SetFloat("MovementSpeed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+            Debug.Log("Sprinting");
+        }
+        else
+        {
+            CharRigid.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MoveSpeed, CharRigid.velocity.y);
+            CharAnim.SetFloat("MovementSpeed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+        }
 
         if (Input.GetButtonDown("Jump"))
         {
-            if (Physics2D.Linecast(transform.position,
-                transform.position + new Vector3(0, RayLength, 0),
-                1 << LayerMask.NameToLayer("Floor")))
+            if (Physics2D.Linecast(transform.position,transform.position + new Vector3(0, RayLength, 0),1 << LayerMask.NameToLayer("Floor")))
             {
                 CharRigid.AddForce(new Vector2(0, JumpStrength));
-                // AudioManager.AnimAudioManager.PlaySFX("Jump");
+                CharAnim.SetBool("Jumping", true);
+                SFXScrpt.AudioManager.PlaySFX("Jump");
             }
         }
+
+        if(Input.GetMouseButtonDown(1)) // attack
+        {
+            CharAnim.SetBool("Attacking", true);
+            SFXScrpt.AudioManager.PlaySFX("Jump");
+            Debug.Log("Attacking");
+        }
+
     }
+
+    private void FlipChar() // Makes char face the direction they're walking
+    {
+        if (CharRigid.velocity.x > 0 && transform.localScale.x < 0 || CharRigid.velocity.x < 0 && transform.localScale.x > 0)
+        {
+            Vector2 VEC = transform.localScale;
+            VEC.x *= -1;
+            transform.localScale = VEC;
+        }
+    }
+
+    
 }
---
+++ DCA525 Mobile Game Fundamentals/Assets/SFXScrpt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXScrpt : MonoBehaviour {
+
+    public GameObject SFXManager;
+    public static SFXScrpt AudioManager;
+
+    public List<string> SFXName = new List<string>();
+    public List<AudioClip> SFXAudio = new List<AudioClip>();
+    public Dictionary<string, AudioClip> SFX_Libairy = new Dictionary<string, AudioClip>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < SFXName.Count; i++)
+        {
+            SFX_Libairy.Add(SFXName[i], SFXAudio[i]);
+        }
+
+        AudioManager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlaySFX(string Name)
+    {
+        if (SFX_Libairy.ContainsKey(Name))
+        {
+            GameObject SFX = Instantiate(SFXManager);
+            AudioSource SRC = SFX.GetComponent<AudioSource>();
+            SRC.clip = SFX_Libairy[Name];
+            SRC.Play();
+
+            Destroy(SFX, SRC.clip.length);
+        }
+    }
+
+ 
+}
